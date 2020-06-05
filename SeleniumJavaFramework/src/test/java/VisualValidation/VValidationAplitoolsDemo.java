99e9f75ccff51624b099937d85d5bcfb3181a7dc
lenovo <lenovo@LAPTOP-NJ6KKULS>
2020-06-05 21:24:01 +0530

    Third commit

---
+++ SeleniumJavaFramework/src/test/java/VisualValidation/VValidationAplitoolsDemo.java
@@ -0,0 +1,7 @@
+package VisualValidation;
+//not getting code
+
+public class VValidationAplitoolsDemo {
+	/*https://mvnrepository.com/artifact/com.applitools/eyes-common-java3/3.164.1
+	 * https://applitools.com/helloworld/*/
+}
